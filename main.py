5b882b0f43636efb6fd761eb4ee2deafbfd11003
rootofmylife <dotrananhduc@gmail.com>
2021-04-05 00:32:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,30 @@ import keyboard
 
 from tkinter import *
 
-mapping_scheme = {
-    'c': 'k',
+mapping_consonant_type_one = {
     'q': 'k',
     'd': 'z',
-    'gi': 'z',
     'r': 'z',
-    'ch': 'c', # ch -> c
+    'x': 's',
+}
+
+mapping_consonant_type_two = {
     'tr': 'c',
-    'ng': 'w',
-    'wh': 'w', # ngh -> w
+    'gi': 'z',
     'nh': 'ń',
     'ph': 'f',
     'kh': 'x',
-    'x': 's',
     'th': 'q',
+}
+
+mapping_consonant_type_three = {
+    'c': 'k',
+    'ch': 'c',
+    'ng': 'w',
+    'ngh': 'w',
+}
+
+mapping_vowel = {
     'ia': 'iê', 'ìa': 'iề', 'ía': 'iế', 'ỉa': 'iể', 'ĩa': 'iễ', 'ịa': 'iệ',
     'uan': 'ua', 'uản': 'ủa', 'uàn': 'ùa', 'uán': 'úa', 'uãn': 'ũa', 'uạn': 'ụa',
     'uang': 'ua', 'uàng': 'ùa', 'uáng': 'úa', 'uảng': 'ủa', 'uãng': 'ũa', 'uạng': 'ụa',
@@ -30,27 +39,31 @@ mapping_scheme = {
     'uye6': 'uiê', 'uiê2': 'uiề', 'uiê1': 'uiế', 'uiê3': 'uiể', 'uiê4': 'uiễ', 'uiê5': 'uiệ' # Telex: uyê - > uiê
 }
 
-break_word_character = ['space', 'enter', 'delete', 'backspace', '[', ']', '\\', ';', "'", ',', '.', '/', '`', '-', '=']
 current_word = ''
 
 def on_press_keyboard(event):
-    global current_word, waiting_scheme_flag
-    if event.name in break_word_character:
-        current_word = ''
-    # elif event.name == 'backspace':
-    #     if len(current_word) > 0:
-    #         current_word = current_word[:-1]
-    
-    if len(str(event.name)) == 1 and re.match("^[A-Za-z0-9_-]*$", event.name):
-        current_word = current_word + str(event.name)
-        print(current_word)
+    global current_word
 
-    if len(str(current_word)) > 0 and len(str(current_word)) < 7:
-        if current_word.lower() in mapping_scheme:
-            for x in range(len(current_word)):
-                keyboard.send('backspace')
-            keyboard.write(str(mapping_scheme[current_word]))
-            current_word = mapping_scheme[current_word]
+    if str(event.name).isalpha() or str(event.name).isdigit():
+        if len(current_word) > 5:
+            current_word = ''
+        else:
+            current_word = current_word + str(event.name)
+
+        if current_word in mapping_consonant_type_one:
+            keyboard.send('backspace') # remove last character
+            keyboard.write(str(mapping_consonant_type_one[current_word]))
+            current_word = ''
+        elif current_word in mapping_consonant_type_two:
+            keyboard.send('backspace') # remove last character
+            keyboard.send('backspace') # remove second last character
+            keyboard.write(str(mapping_consonant_type_two[current_word]))
+            current_word = ''
+        elif current_word in mapping_consonant_type_three:
+            keyboard.write(str(mapping_consonant_type_three[current_word]))
+            current_word = ''
+    else:
+        current_word = ''
 
 keyboard.on_press(on_press_keyboard)
 
