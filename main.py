71a39e9453b893cf487a8df2ce4f6faa189e399c
rootofmylife <dotrananhduc@gmail.com>
2021-04-05 11:27:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -99,5 +99,64 @@ keyboard.add_abbreviation('uye3', 'uiể', match_suffix=True)
 keyboard.add_abbreviation('uye4', 'uiễ', match_suffix=True)
 keyboard.add_abbreviation('uye5', 'uiệ', match_suffix=True)
 
+# uan -> ua
+keyboard.add_abbreviation('uan', 'ua', match_suffix=True)
+# VNI
+keyboard.add_abbreviation('uanf', 'ùa', match_suffix=True)
+keyboard.add_abbreviation('uans', 'úa', match_suffix=True)
+keyboard.add_abbreviation('uanr', 'ủa', match_suffix=True)
+keyboard.add_abbreviation('uanx', 'ũa', match_suffix=True)
+keyboard.add_abbreviation('uanj', 'ụa', match_suffix=True)
+# Telex
+keyboard.add_abbreviation('uan2', 'ùa', match_suffix=True)
+keyboard.add_abbreviation('uan1', 'úa', match_suffix=True)
+keyboard.add_abbreviation('uan3', 'ủa', match_suffix=True)
+keyboard.add_abbreviation('uan4', 'ũa', match_suffix=True)
+keyboard.add_abbreviation('uan5', 'ụa', match_suffix=True)
+
+# uang -> ua
+keyboard.add_abbreviation('uang', 'ua', match_suffix=True)
+# VNI
+keyboard.add_abbreviation('uangf', 'ùa', match_suffix=True)
+keyboard.add_abbreviation('uangs', 'úa', match_suffix=True)
+keyboard.add_abbreviation('uangr', 'ủa', match_suffix=True)
+keyboard.add_abbreviation('uangx', 'ũa', match_suffix=True)
+keyboard.add_abbreviation('uangj', 'ụa', match_suffix=True)
+# Telex
+keyboard.add_abbreviation('uang2', 'ùa', match_suffix=True)
+keyboard.add_abbreviation('uang1', 'úa', match_suffix=True)
+keyboard.add_abbreviation('uang3', 'ủa', match_suffix=True)
+keyboard.add_abbreviation('uang4', 'ũa', match_suffix=True)
+keyboard.add_abbreviation('uang5', 'ụa', match_suffix=True)
+
+# uai -> ua
+keyboard.add_abbreviation('uai', 'ua', match_suffix=True)
+# VNI
+keyboard.add_abbreviation('uaif', 'ùa', match_suffix=True)
+keyboard.add_abbreviation('uais', 'úa', match_suffix=True)
+keyboard.add_abbreviation('uair', 'ủa', match_suffix=True)
+keyboard.add_abbreviation('uaix', 'ũa', match_suffix=True)
+keyboard.add_abbreviation('uaij', 'ụa', match_suffix=True)
+# Telex
+keyboard.add_abbreviation('uai2', 'ùa', match_suffix=True)
+keyboard.add_abbreviation('uai1', 'úa', match_suffix=True)
+keyboard.add_abbreviation('uai3', 'ủa', match_suffix=True)
+keyboard.add_abbreviation('uai4', 'ũa', match_suffix=True)
+keyboard.add_abbreviation('uai5', 'ụa', match_suffix=True)
+
+# uay -> ua
+keyboard.add_abbreviation('uay', 'ua', match_suffix=True)
+# VNI
+keyboard.add_abbreviation('uayf', 'ùa', match_suffix=True)
+keyboard.add_abbreviation('uays', 'úa', match_suffix=True)
+keyboard.add_abbreviation('uayr', 'ủa', match_suffix=True)
+keyboard.add_abbreviation('uayx', 'ũa', match_suffix=True)
+keyboard.add_abbreviation('uayj', 'ụa', match_suffix=True)
+# Telex
+keyboard.add_abbreviation('uay2', 'ùa', match_suffix=True)
+keyboard.add_abbreviation('uay1', 'úa', match_suffix=True)
+keyboard.add_abbreviation('uay3', 'ủa', match_suffix=True)
+keyboard.add_abbreviation('uay4', 'ũa', match_suffix=True)
+keyboard.add_abbreviation('uay5', 'ụa', match_suffix=True)
 while True:
     pass
